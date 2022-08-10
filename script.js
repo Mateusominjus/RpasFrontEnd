1af9b0c456082aba8e9fa54e2a50c5b0b6ad5b8d
mateusmoutinho <mateusmoutinho01@gmail.com>
2022-08-10 04:33:32 -0300

    att

---
+++ script.js
@@ -0,0 +1,8 @@
+
+
+function pesquisar(){
+    let senha = document.getElementById("senha").value;
+    let ambiente = document.getElementById("ambiente").value;
+    let processo = document.getElementById("processo").value;
+    
+}
