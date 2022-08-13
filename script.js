b7e560e3d02cc4c594a1927110d44546584e095c
mateusmoutinho <mateusmoutinho01@gmail.com>
2022-08-13 13:39:57 -0300

    att

--- script.js
+++ script.js
@@ -13,15 +13,17 @@ function seta_lista_de_documentos(resposta){
         seta_documento(lista_de_documentos.selectedIndex)                
     }
 }
+
 function seta_dados_do_processo(resposta){
      dados_do_processo = document.getElementById("dados_do_processo")
      delete resposta.processo.json_plataforma
      dados_do_processo.innerHTML = JSON.stringify(resposta.processo, undefined, 4)
 }
-function pesquisar(){
+
+
+function pesquisar_processo(processo){
     let senha = document.getElementById("senha").value;
     let ambiente = document.getElementById("ambiente").value;
-    let processo = document.getElementById("processo").value;
     
     //fetch with cors 
     
@@ -53,3 +55,52 @@ function pesquisar(){
 
 
 
+function pesquisar_todos_processos(){
+
+    let senha = document.getElementById("senha").value;
+
+
+    let ambiente = document.getElementById("ambiente")
+    ambiente = ambiente.options[ambiente.selectedIndex].value
+
+    //bate na api
+    fetch(
+        'https://lfklzr5jsotrttsdjaymmzky7q0tlvtn.lambda-url.us-east-1.on.aws/todos_processos',
+     {
+        mode: 'cors',
+        headers: {
+          'Access-Control-Allow-Origin':'*',
+          'senha': senha,
+           'ambiente': ambiente
+        }
+     }
+    ).then(response =>response.text()).then(data =>{
+        
+        try{
+            var processos = JSON.parse(data)
+        }catch(e){
+            //Caso ocorra algum erro, exibe o erro na tela
+            alert(data)
+            return
+        }
+        select_processos = document.getElementById("processos")
+        //reseta tudo
+        select_processos.innerHTML = ''
+        lista_de_documentos = document.getElementById("lista_de_documentos")
+        lista_de_documentos.innerHTML = ''
+        dados_do_processo = document.getElementById("dados_do_processo")
+        dados_do_processo.innerHTML = ''
+
+        processos.map((processo) => {
+            opcao = document.createElement("option")
+            opcao.innerHTML = processo
+            select_processos.appendChild(opcao)
+
+        })
+        select_processos.onchange= () => {
+            let processo = select_processos.options[select_processos.selectedIndex].text
+            pesquisar_processo(processo)
+        }
+    })
+
+}
